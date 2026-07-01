5c8e24a5d6fac9c2415a7057e80e59bed94ee093
Heisenbobol <lorenzoaag@outlook.com>
2026-07-01 02:17:49 -0300

    Refactor ast.h for improved readability

--- ast.h
+++ ast.h
@@ -2,26 +2,16 @@
 #define AST_H
 
 typedef enum {
-    N_PROG,
-    N_LISTA,
-    N_SET,
-    N_ACAO,
-    N_ALERTA,
-    N_IF,
-    N_COND
+    N_PROG, N_LISTA, N_SET, N_ACAO, N_ALERTA, N_IF
 } TipoNo;
 
 typedef struct No {
     TipoNo tipo;
     char *val;
-
-    struct No *a;
-    struct No *b;
-    struct No *c;
+    struct No *a, *b, *c;
 } No;
 
 No* novoNo(TipoNo t, char *v, No *a, No *b, No *c);
-
 void gerar(No *n);
 
 #endif
